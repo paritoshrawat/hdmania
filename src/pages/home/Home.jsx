a3eba6e48c8bb3bd0cb01b6b0ba0da440043d8aa
unknown <julmiparvez@gmail.com>
2023-06-19 13:20:01 +0530

    fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./style.scss"
-// import HeroBanner from './heroBanner/HeroBanner';
+
 import Trending from './trending/Trending';
 import Popular from './popular/Popular';
 import TopRated from './topRated/TopRated';
+import HeroBanner from './heroBanner/heroBanner';
 
 
 
