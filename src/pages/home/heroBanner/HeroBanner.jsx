e61ccfbc2ee1b381b58a66b0ae73f89efa0ac288
unknown <julmiparvez@gmail.com>
2023-06-19 12:15:28 +0530

    hdmania app completed

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import "./style.scss"
+import { useNavigate } from 'react-router-dom';
+import useFetch from '../../../hooks/UseFetch';
+import { useSelector } from 'react-redux';
+import Img from '../../../components/lazyLoadingImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+
+const HeroBanner = () => {
+  const [background,setBackground] = useState("");
+  const [query,setQuery] = useState("");
+  const navigate = useNavigate();
+  const {url} = useSelector((state)=>state.home);
+const {data,loading } = useFetch("/movie/upcoming");
+
+  useEffect(()=>{
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg)
+
+  },[data])
+
+  const searchQueryHandler = (event)=>{
+    if(event.key === "Enter" && query.length>0){
+      navigate(`/search/${query}`)
+
+    }
+
+  }
+
+  return (
+    <div className='heroBanner'>
+
+   { !loading &&   <div className="backdrop_img">
+        <Img src={background}/>
+      </div>
+      }
+
+      <div className="opacity-layer"></div>
+
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">welcome</span>
+          <span className="subTitle">
+            Millions of movies,Tv Shows and webseries
+             to discover. 
+            Explore Now.
+          </span>
+          <div className="searchInput">
+            <input 
+            type="text"
+            placeholder='Search For Movies or Tv Shows...'
+            onChange={(e)=>setQuery(e.target.value)}
+            onKeyUp={searchQueryHandler} />
+            <button>Search</button>
+          </div>
+        </div>
+      
+            </ContentWrapper>
+    </div>
+  )
+}
+
+export default HeroBanner
